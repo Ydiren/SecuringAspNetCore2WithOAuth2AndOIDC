ebf4c5d48d7f5ee8288e2ae5330d5f84741438ef
ydiren <andymg@gmail.com>
2019-03-14 17:25:24 +0000

    Implemented first pass of IdentityServer4 and utilised the QuickStart.UI project

---
+++ Progress/ImageGallery/src/Marvin.IDP/Config.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using Microsoft.AspNetCore.Http;
+
+namespace Marvin.IDP
+{
+    public static class Config
+    {
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+                   {
+                       new TestUser
+                       {
+                           SubjectId = "BFF0AF2A-6230-4408-99F0-8A28C203D711",
+                           Username = "Frank",
+                           Password = "password",
+
+                           Claims = new List<Claim>
+                                    {
+                                        new Claim("given_name", "Frank"),
+                                        new Claim("family_name", "Underwood")
+                                    }
+                       },
+                       new TestUser
+                       {
+                           SubjectId = "82B616B2-CB65-43F3-9F1B-D4EBBF27ED70",
+                           Username = "Claire",
+                           Password = "password",
+
+                           Claims = new List<Claim>
+                                    {
+                                        new Claim("given_name", "Claire"),
+                                        new Claim("family_name", "Underwood")
+                                    }
+                       }
+                   };
+        }
+
+        /// <summary>
+        /// Identity related resources (scopes)
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+                   {
+                       new IdentityResources.OpenId(),
+                       new IdentityResources.Profile()
+                   };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>();
+        }
+    }
+}
