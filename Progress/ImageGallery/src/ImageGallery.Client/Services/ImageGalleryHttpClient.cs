763cd57dc2ef701017c9a7f1a9d69dcffa34ae44
Andy Green <andymg@gmail.com>
2019-03-16 12:36:22 +0000

    Restroring access to the API to authorized users

--- Progress/ImageGallery/src/ImageGallery.Client/Services/ImageGalleryHttpClient.cs
+++ Progress/ImageGallery/src/ImageGallery.Client/Services/ImageGalleryHttpClient.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace ImageGallery.Client.Services
 {
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
     public class ImageGalleryHttpClient : IImageGalleryHttpClient
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
@@ -17,7 +20,20 @@ namespace ImageGallery.Client.Services
         }
         
         public async Task<HttpClient> GetClient()
-        {      
+        {
+            // Get the current HttpContext to access the tokens
+            var currentContext = _httpContextAccessor.HttpContext;
+
+            // Get the access token
+            var accessToken = await currentContext.GetTokenAsync(
+                OpenIdConnectParameterNames.AccessToken);
+
+            if (!string.IsNullOrWhiteSpace(accessToken))
+            {
+                // Set bearer token
+                _httpClient.SetBearerToken(accessToken);
+            }
+
             _httpClient.BaseAddress = new Uri("https://localhost:44366/");
             _httpClient.DefaultRequestHeaders.Accept.Clear();
             _httpClient.DefaultRequestHeaders.Accept.Add(
